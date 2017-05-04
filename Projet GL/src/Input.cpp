09d7104f25cdbfff09fb8abffc32bf5215460ff6
Thomas-Guzik <thomas.guzik@laposte.net>
2017-05-04 10:49:27 +0200

    Update Input.cpp
    Ajout de nouvelles méthodes, getCursorPosition setCursorPosition getTermSize inInt

--- Projet GL/src/Input.cpp
+++ Projet GL/src/Input.cpp
@@ -40,11 +40,9 @@ unsigned char Input::keyboard() {
 			}
 			else return ESC;
 		}
-		if(c == UP || c == RIGHT || c == DOWN || c == LEFT || c == ENT) return c;
+		else if(c == UP || c == RIGHT || c == DOWN || c == LEFT || c == ENT) return c;
 		else if(c == '/') {
-			printf("ok");
 			printf("\033[s");
-			printf("\n\n\n");
 			unsigned char s[15];
 			char i = 0;
 			printf("/");
@@ -68,8 +66,79 @@ unsigned char Input::keyboard() {
 			if(s[0] == 'q')
 				return QUIT;
 		}
-		else if(c == UP || c == RIGHT || c == DOWN || c == LEFT)
-			return c;
 	} while(true);
+}
 
+Cord Input::getCursorPosition() {
+	Cord cursor;
+	printf("\033[6n");
+	unsigned char c = getchar(); //ESC
+	getchar();
+	cursor.x = getchar() - '0';
+	c = getchar();
+	while(c != ';') {
+		cursor.x = cursor.x*10 + (c - '0');
+		c = getchar();
+	}
+	cursor.y = getchar() - '0';
+	c = getchar();
+	while(c != 'R') {
+		cursor.y = cursor.y*10 + (c - '0');
+		c = getchar();
+	}
+	return cursor;
+}
+
+void Input::setCursorPosition(Cord position) {
+	printf("\033[%d;%dH",position.x, position.y);
+}
+
+void Input::setCursorPosition(int x, int y) {
+	printf("\033[%d;%dH", x, y);
+}
+
+void Input::printCord(Cord cord) {
+	printf("x:%d y:%d", cord.x, cord.y);
+}
+Cord Input::getTermSize() {
+	Cord size;
+	printf("\033[s");
+	printf("\033[999;999H");
+	size = getCursorPosition();
+	printf("\033[u");
+	return size;
+}
+
+int Input::inInt(){
+	bool negative = false;
+	int i = 0;
+	int a =  0;
+	unsigned char c = 0;
+	
+	c = getchar();
+	do {
+		if(i == 0 && c == '-') {
+			negative = true;
+			printf("-");
+			i++;
+		}
+		if(a < 999999 && c >= '0' && c <= '9') {
+			a = a*10 + (c -'0');
+			printf("%c",c);
+			i++;
+		}
+		if(c == 127 && i > 0) {
+			if(i > 1 || negative == false) {
+				a /= 10;
+			}
+			else {
+				negative = false;
+			}
+			printf("\033[1D \033[1D");
+			i--;
+		}
+		c = getchar();
+	}while(c != ENT);
+	if(negative) a *= -1;
+	return a;
 }
